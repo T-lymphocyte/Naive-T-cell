b947360e0115200654ff0103215dccafe010a0a9
T-lymphocyte <66730132+T-lymphocyte@users.noreply.github.com>
2020-07-09 21:58:38 +0800

    Update main.py

--- naivetcell/main.py
+++ naivetcell/main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import json
 import os
+from discord.ext.commands import bot
 with open("cmds/setting.json", mode='r', encoding='utf8') as jfile:
     jdata = json.load(jfile)
 
@@ -12,16 +13,22 @@ async def on_ready():
     print('ready')
 @bot.command()
 async def load(ctx, extension):
-    bot.load_extension(f'cmds.{extension}')
-    await ctx.send(f'Loaded {extension}')
+    author = bot.get_user(730769863378862130)
+    if ctx.author == author:
+        bot.load_extension(f'cmds.{extension}')
+        await ctx.send(f'Loaded {extension}')
 @bot.command()
 async def unload(ctx, extension):
-    bot.unload_extension(f'cmds.{extension}')
-    await ctx.send(f'Unloaded {extension}')
+    author = bot.get_user(730769863378862130)
+    if ctx.author == author:
+        bot.unload_extension(f'cmds.{extension}')
+        await ctx.send(f'Unloaded {extension}')
 @bot.command()
 async def reload(ctx, extension):
-    bot.reload_extension(f'cmds.{extension}')
-    await ctx.send(f'Loaded {extension}')
+    author = bot.get_user(730769863378862130)
+    if ctx.author == author:
+        bot.reload_extension(f'cmds.{extension}')
+        await ctx.send(f'Loaded {extension}')
 for filename in os.listdir('cmds'):
     if filename.endswith('.py'):
         bot.load_extension(f'cmds.{filename[:-3]}')
